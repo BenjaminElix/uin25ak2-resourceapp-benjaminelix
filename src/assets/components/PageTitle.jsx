d36809bc7c0f3b8d2c68a7f5f210c8f1008e8172
BenjaminElix <142603383+BenjaminElix@users.noreply.github.com>
2025-03-17 01:41:07 +0100

    endring i beskrivelser som gir mer mening

--- src/assets/components/PageTitle.jsx
+++ src/assets/components/PageTitle.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 
 function PageTitle() {
-  return null; // Deaktiverer rendring fordi det var lettere
+  return null; // Deaktiverer rendring siden tittelen nå håndteres direkte i Resources-komponenten fordi det var enklere
 }
 
 export default PageTitle;
